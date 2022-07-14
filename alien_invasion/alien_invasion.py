3a1b3d41391633699d65b549e0fe6ba595acde0f
Kuro2003 <cuongnguyengl2003@gmail.com>
2022-07-14 21:54:08 +0700

    create fire a bullets

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,29 +1,40 @@
+from hashlib import new
 import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
 # Overall class to manage game assets and behavior. 
     def __init__(self):
 
-    # Initialize the game, and create game resources.
+        # Initialize the game, and create game resources.
         pygame.init()
-        pygame.display.set_caption("Aline Invasion")  
+        pygame.display.set_caption("Alien Invasion")  
         self.settings = Settings() 
         self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
-    # set background color
+        # set background color
         self.bg_color = (230,230,230)
-    
+
+        # Call ship
         self.ship = Ship(self)
+        # Call bullets
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
     # Start the main loop for the game
         while True:
             self._check_events()
             self.ship.update()
-            self._update_screen()
+            self.bullets.update()
 
+            # Get rid of bullets that have disappear
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+                
+            self._update_screen()
 
             pygame.display.flip()
     def _check_events(self):
@@ -32,24 +43,45 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        # Move the ship to the right
-                        self.ship.moving_right = True
-                    elif event.key == pygame.K_LEFT:
-                        # Move the ship to the left
-                        self.ship.moving_left = True
-
+                    self._check_keydown_events(event)
                 elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = False
-                    elif event.key == pygame.K_LEFT:
-                        self.ship.moving_left = False
+                    self._check_keyup_events(event)
+    
+    def _check_keydown_events(self,event):
+        # Respond to keypress.
+        if event.key == pygame.K_RIGHT:
+            # Move the ship to the right
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            # Move the ship to the left
+            self.ship.moving_left = True
                 
+    def _check_keyup_events(self,event):
+        # Rcespond to release.
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+        elif event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
     def _update_screen(self):
         # update images on the screen, and flip to the new screen.
         # Redraw the screen during each pass through the loop
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        pygame.display.flip()
+    
+    def _fire_bullet(self):
+        """ Create a new bullet and add it to the bullets group """
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
 
     
 if __name__ == "__main__":
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -2,7 +2,15 @@ class Settings:
     # A class to store all settings for Alien Invasion.
     # Screen settings
     def __init__(self):
+        # Screen setting
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (255,255,230)
         self.ship_speed = 1.5
+
+        # Bullet setting
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
+        self.bullets_allowed = 3
