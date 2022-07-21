9cdd1109e89de6b7bc35afb9d4e0347eff1da454
Kuro2003 <cuongnguyengl2003@gmail.com>
2022-07-21 23:32:45 +0700

    display high_score

--- alien_invasion/scoreboard.py
+++ alien_invasion/scoreboard.py
@@ -1,6 +1,7 @@
 from re import S
 from sqlite3 import PrepareProtocol
 import pygame.font
+from sklearn.feature_selection import SelectKBest
 class Scoreboard:
     """ A class to report scoring information """
     def __init__(self,ai_game):
@@ -13,8 +14,9 @@ class Scoreboard:
         # Font settings for scoring information.
         self.text_color = (30,30,30)
         self.font = pygame.font.SysFont(None,48)
-        # Prepare the initial score immage.
+        # Prepare the initial score images.
         self.prep_score()
+        self.prep_high_score()
     
     def prep_score(self):
         """ Turn the score into a rendered image. """
@@ -26,6 +28,24 @@ class Scoreboard:
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
+    def prep_high_score(self):
+        """ Turn the high score into a rendered image."""
+        high_score = round(self.stats.high_score,-1)
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str,True,self.text_color,self.settings.bg_color)
+
+        # Center the high score at the top of the screen
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
+    def check_high_score(self):
+        """ Check to see if there's a new high score. """
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score    
+            self.prep_high_score()
+
     def show_score(self):
         """ Draw score to the screen. """
-        self.screen.blit(self.score_image,self.score_rect)
+        self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image,self.high_score_rect)
